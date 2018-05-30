5363947f44a5a18981508aa397fdb2370ebce3da
Jaye Li <2238179368@qq.com>
2018-05-30 15:22:16 +0800

    新增我的发布页面框架

---
+++ home/src/main/java/coc/team/home/fragment/AuditedFragment.java
@@ -0,0 +1,24 @@
+package coc.team.home.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import coc.team.home.R;
+
+/**
+ * 我的发布页面, 已审核的fragment
+ */
+public class AuditedFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_unaudited, null);
+
+        return view;
+    }
+}
---
+++ home/src/main/java/coc/team/home/fragment/UnauditedFragment.java
@@ -0,0 +1,23 @@
+package coc.team.home.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import coc.team.home.R;
+
+/**
+ * 我的发布页面，未审核fragment
+ */
+public class UnauditedFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_audited, null);
+
+        return view;
+    }
+}
