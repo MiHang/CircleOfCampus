1a66a7325b985324ce94d892ed4d8b857e04aa24
Jaye Li <2238179368@qq.com>
2018-05-30 13:02:38 +0800

    新增跳转到主页接口方法

--- home/src/main/java/coc/team/home/service/HomeService.java
+++ home/src/main/java/coc/team/home/service/HomeService.java
@@ -1,9 +1,12 @@
 package coc.team.home.service;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 
+import coc.team.home.activity.HomeActivity;
 import coc.team.home.provider.HomeProvider;
 
 /**
@@ -13,4 +16,11 @@ import coc.team.home.provider.HomeProvider;
 public class HomeService implements HomeProvider {
     @Override
     public void init(Context context) {}
+
+    @Override
+    public void toHome(Activity activity) {
+        Intent intent = new Intent(activity, HomeActivity.class);
+        activity.startActivity(intent);
+        activity.finish();
+    }
 }
