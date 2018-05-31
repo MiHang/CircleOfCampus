e5ade0d5a659616a3291d5cee02e283115c3c01c
Alivesssss <1724889709@qq.com>
2018-05-31 18:42:40 +0800

    校园圈页面优化

--- home/src/main/java/coc/team/home/fragment/NoticeActivity.java
+++ home/src/main/java/coc/team/home/fragment/NoticeActivity.java
@@ -5,24 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import coc.team.home.R;
 
 public class NoticeActivity extends AppCompatActivity {
 
-    private ImageView notice;
+    private ImageView backIcon;
+    private TextView headerTitle;
+    private ImageView headerSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notice);
+        setContentView(R.layout.activity_corporation);
 
-        notice = findViewById(R.id.home_campus_notice_1);
-        notice.setOnClickListener(new View.OnClickListener() {
+        headerTitle = (TextView) findViewById(R.id.header_title);
+        headerTitle.setText("校园官方公告");
+
+        headerSearch = (ImageView) findViewById(R.id.header_right_image);
+        headerSearch.setImageResource(R.drawable.search);
+
+        backIcon = findViewById(R.id.header_left_image);
+        backIcon.setVisibility(View.VISIBLE);
+        backIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(NoticeActivity.this,AFragment.class);
-                startActivity(intent);
+                onBackPressed();
             }
         });
     }
