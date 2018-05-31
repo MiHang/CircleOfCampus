67476b0d95dea21d21ec872b1a147092e68dd59b
Alivesssss <1724889709@qq.com>
2018-05-31 10:01:05 +0800

    校园圈页面

--- home/src/main/java/coc/team/home/fragment/AFragment.java
+++ home/src/main/java/coc/team/home/fragment/AFragment.java
@@ -7,6 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.youth.banner.Banner;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import coc.team.home.R;
 
 /**
@@ -15,6 +20,7 @@ import coc.team.home.R;
 
 public class AFragment extends Fragment {
 
+    private List<Integer> images = new ArrayList<>();//声明数组
 
 
     @Override
@@ -22,6 +28,16 @@ public class AFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_a, null);
 
+        Banner banner = (Banner) view.findViewById(R.id.banner);
+        //设置图片加载器
+        banner.setImageLoader(new Lunbotu());
+        //设置图片集合
+        images.add(R.drawable.lunbo01);
+        images.add(R.drawable.lunbo02);
+        images.add(R.drawable.lunbo04);
+        banner.setImages(images);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
 
         return view;
     }
