8f0be6f222165757dbdb8d168fd3db6c7a17f719
Jaye Li <2238179368@qq.com>
2018-05-31 17:01:32 +0800

    修改标题栏返回图标大小，构建更多社团公告页面

--- home/src/main/java/coc/team/home/fragment/CorporationActivity.java
+++ home/src/main/java/coc/team/home/fragment/CorporationActivity.java
@@ -1,28 +1,36 @@
 package coc.team.home.fragment;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import coc.team.home.R;
 
 public class CorporationActivity extends AppCompatActivity {
 
-    private ImageView corporation;
+    private ImageView backIcon;
+    private TextView headerTitle;
+    private ImageView headerSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_corporation);
 
-        corporation = findViewById(R.id.home_corporation_notice_1);
-        corporation.setOnClickListener(new View.OnClickListener() {
+        headerTitle = (TextView) findViewById(R.id.header_title);
+        headerTitle.setText("社团公告");
+
+        headerSearch = (ImageView) findViewById(R.id.header_right_image);
+        headerSearch.setImageResource(R.drawable.search);
+
+        backIcon = findViewById(R.id.header_left_image);
+        backIcon.setVisibility(View.VISIBLE);
+        backIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CorporationActivity.this,AFragment.class);
-                startActivity(intent);
+                onBackPressed();
             }
         });
     }
