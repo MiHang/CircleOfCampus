f8d60f3bd0bbdd6fd2509dd43b71ff18e11004fa
Administrator <575566133@qq.com>
2018-05-31 20:42:16 +0800

    完成登陆界面

--- login/src/main/java/coc/team/login/ButtonEditText.java
+++ login/src/main/java/coc/team/login/ButtonEditText.java
@@ -6,9 +6,3 @@ import android.widget.EditText;
 /**
  * Created by Administrator on 2018/5/28/028.
  */
-
-public class ButtonEditText extends EditText {
-    public ButtonEditText(Context context) {
-        super(context);
-    }
-}
